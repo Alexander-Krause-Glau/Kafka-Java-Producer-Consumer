6aad44010c2d74951e368b9d3bb48092cd6efad0
Alexander Krause <akr@informatik.uni-kiel.de>
2018-10-01 16:42:51 +0200

    init

---
+++ src/main/java/org/krause/kafka/ConsumerExample.java
@@ -0,0 +1,76 @@
+package org.krause.kafka;
+
+import com.google.gson.Gson;
+import java.io.UnsupportedEncodingException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class ConsumerExample {
+
+  public static void main(String[] args) throws InterruptedException, UnsupportedEncodingException {
+
+    // https://kafka.apache.org/20/javadoc/index.html?org/apache/kafka/clients/consumer/KafkaConsumer.html
+
+    Properties properties = new Properties();
+    properties.put("bootstrap.servers", "localhost:9092");
+    properties.put("group.id", "test");
+    properties.put("enable.auto.commit", "true");
+    properties.put("auto.commit.interval.ms", "1000");
+    properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+    properties.put("value.deserializer",
+        "org.apache.kafka.common.serialization.StringDeserializer");
+
+    properties.put("group.id", "example-group");
+
+    properties.put("kafka.topic", "example-topic");
+
+    runMainLoop(args, properties);
+  }
+
+  static void runMainLoop(String[] args, Properties properties)
+      throws InterruptedException, UnsupportedEncodingException {
+
+    // Create Kafka producer
+    KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+    try {
+
+      consumer.subscribe(Arrays.asList(properties.getProperty("kafka.topic")));
+
+      System.out.println("Subscribed to topic " + properties.getProperty("kafka.topic"));
+
+      while (true) {
+        ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+        for (ConsumerRecord<String, String> record : records) {
+          System.out.printf("partition = %s, offset = %d, key = %s, value = %s\n",
+              record.partition(), record.offset(), record.key(),
+              decodeMsg(record.value()).getData());
+        }
+
+      }
+    }
+
+    finally {
+      consumer.close();
+    }
+  }
+
+  public static Message decodeMsg(String json) throws UnsupportedEncodingException {
+
+    Gson gson = new Gson();
+
+    Message msg = gson.fromJson(json, Message.class);
+
+    byte[] encodedData = Base64.getDecoder().decode(msg.getData());
+    msg.setData(new String(encodedData, "UTF-8"));
+
+    return msg;
+  }
+
+}
---
+++ src/main/java/org/krause/kafka/ProducerExample.java
@@ -0,0 +1,86 @@
+package org.krause.kafka;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.io.UnsupportedEncodingException;
+import java.sql.Timestamp;
+import java.util.Base64;
+import java.util.Properties;
+import java.util.Random;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class ProducerExample {
+  
+  // Credit: https://gist.github.com/kleysonr/d76df87479cc884818ebe870d297d7e5
+
+  public static void main(String[] args) throws InterruptedException, UnsupportedEncodingException {
+
+    // https://kafka.apache.org/20/javadoc/index.html?org/apache/kafka/clients/producer/KafkaProducer.html
+
+    Properties properties = new Properties();
+    properties.put("bootstrap.servers", "localhost:9092");
+    properties.put("acks", "all");
+    properties.put("retries", "1");
+    properties.put("batch.size", "16384");
+    properties.put("linger.ms", "1");
+    properties.put("max.request.size", "2097152");
+    properties.put("buffer.memory", 33554432);
+    properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+    properties.put("kafka.topic", "example-topic");
+
+    runMainLoop(args, properties);
+  }
+
+  static void runMainLoop(String[] args, Properties properties)
+      throws InterruptedException, UnsupportedEncodingException {
+
+    // Create Kafka producer
+    KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+    try {
+
+      while (true) {
+
+        Thread.sleep(1000);
+        String id = "device-" + getRandomNumberInRange(1, 5);
+        producer.send(new ProducerRecord<String, String>(properties.getProperty("kafka.topic"), id,
+            getMsg(id)));
+
+      }
+
+    } finally {
+
+      producer.close();
+
+    }
+
+  }
+
+  public static String getMsg(String id) throws UnsupportedEncodingException {
+
+    Gson gson = new Gson();
+
+    String timestamp = new Timestamp(System.currentTimeMillis()).toString();
+
+    JsonObject obj = new JsonObject();
+    obj.addProperty("id", id);
+    obj.addProperty("timestamp", timestamp);
+    obj.addProperty("data",
+        Base64.getEncoder().encodeToString("Test data: This is a simple String".getBytes("UTF-8")));
+    String json = gson.toJson(obj);
+
+    return json;
+
+  }
+
+  private static int getRandomNumberInRange(int min, int max) {
+
+    Random r = new Random();
+    return r.ints(min, (max + 1)).findFirst().getAsInt();
+
+  }
+
+}
